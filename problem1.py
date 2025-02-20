f514d1f13ee5d72dbbe095330c35ee891bde716d
Jamal-Savage <savagejamal0@gmail.com>
2025-02-20 23:56:55 +0000

    random

--- problem1.py
+++ problem1.py
@@ -1,5 +1,8 @@
+#take user input
 String = input("Enter a string to be reversed: ")
 
+#take the reversed string into a new variable
 reversedString = String[::-1]
 
+#print reversed string
 print(f'Your reversed String is: {reversedString}')
